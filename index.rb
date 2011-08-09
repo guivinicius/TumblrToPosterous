f655b7fdcb646cdd9118cf751edf5f2aeda09314
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2011-08-09 11:20:23 -0300

    indentation fix

--- index.rb
+++ index.rb
@@ -8,11 +8,11 @@ require 'posterous'
 require 'open-uri'
 
 def give_me_tags(tags)
-	tags.nil? ? "" : tags.join(",")
+  tags.nil? ? "" : tags.join(",")
 end
 
 def give_me_a_image(image_url)
-	open('image.png', 'wb') { |file| file << open(image_url).read }
+  open('image.png', 'wb') { |file| file << open(image_url).read }
 end
 
 Posterous.config = {
@@ -28,12 +28,12 @@ blog = Site.find('guivinicius')
 
 # A nice name for a nice hash.
 posterous_post_params = {:title => '', 
-												 :body => '', 
-						 						 :tags => '',
-						 						 :autopost => true,
-						 						 :display_date => Time.now.to_s,
-						 						 :source => 'http://guivinicius.tumblr.com',
-						 						 :media => []}
+                         :body => '', 
+                         :tags => '',
+                         :autopost => true,
+                         :display_date => Time.now.to_s,
+                         :source => 'http://guivinicius.tumblr.com',
+                         :media => []}
 
 response = HTTParty.get('http://www.guivinicius.com.br/api/read/json?debug=1')
 obj = Crack::JSON.parse(response.body)
@@ -41,17 +41,17 @@ obj = Crack::JSON.parse(response.body)
 # My precious posts (NOT!)
 obj['posts'].each do |p|
 
-	case p['type']
-		when "regular"
-			params = {:title => p['regular-title'], :body => p['regular-body'], :tags => give_me_tags(p['tags'])}
-		when "photo"
-			image = give_me_a_image(p['photo-url-500'])
-			params = {:title => p['slug'].titleize, :media => [image]}
-		when "link"
-			params = {:title => p['link-text'], :body => p['link-url']}
-	end
+  case p['type']
+    when "regular"
+      params = {:title => p['regular-title'], :body => p['regular-body'], :tags => give_me_tags(p['tags'])}
+    when "photo"
+      image = give_me_a_image(p['photo-url-500'])
+      params = {:title => p['slug'].titleize, :media => [image]}
+    when "link"
+      params = {:title => p['link-text'], :body => p['link-url']}
+  end
 
-	blog.posts.create(posterous_post_params.merge(params))
+  blog.posts.create(posterous_post_params.merge(params))
 end
 
 # Leave my ruby file alone!!!
